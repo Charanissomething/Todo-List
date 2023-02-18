504687764ff0703885672b668699ab8d34fff19a
Charanissomething <ramasaicharangudapureddy@gmail.com>
2023-02-18 21:54:12 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 9030;
+
+app.use("/", function(req, res) {
+    res.sendFile(__dirname + "/frontend/index.html");
+});
+
+
+app.get("/api/todos", function(req, res) {
+    res.json([
+        { name: todo1, iscompleted: true }, { name: todo1, iscompleted: true }, { name: todo1, iscompleted: true }
+    ])
+});
+
+app.listen(port, function() {
+    console.log("Sever running on http://localhost:" + port);
+});
